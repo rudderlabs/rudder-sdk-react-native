3f2a7e8c03b95c07b0f39afd25d0ed77f7301560
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2023-06-20 18:04:29 +0530

    fix(rudder-integration-amplitude-react-native): amplitude module synchronisation issue

--- libs/rudder-integration-amplitude-react-native/ios/RudderIntegrationAmplitudeReactNative.m
+++ libs/rudder-integration-amplitude-react-native/ios/RudderIntegrationAmplitudeReactNative.m
@@ -6,9 +6,10 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(setup)
+RCT_EXPORT_METHOD(setup:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
     [RNRudderAnalytics addIntegration:[RudderAmplitudeFactory instance]];
+    resolve(nil);
 }
 
 @end
