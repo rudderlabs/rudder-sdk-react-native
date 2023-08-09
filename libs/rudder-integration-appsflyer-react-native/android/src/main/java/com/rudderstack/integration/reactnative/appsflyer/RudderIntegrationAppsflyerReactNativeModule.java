1b911b1b66ed76c3c53d9fd8638d755d9b8a3113
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2023-08-09 21:59:48 +0530

    fix: appsflyer native event emitter pop up issue (#228)
    * fix(rudder-integration-appsflyer-react-native): fix new NativeEventEmitter popu-up error
    
    Solve: NativeEventEmitter was called with a nonnull argument without the required addListener method
    
    * chore(rudder-sdk-react-native-monorepo): update package-lock.json
    
    * chore(rudder-sdk-react-native-monorepo): remove package-lock.json
    
    * chore(rudder-sdk-react-native-monorepo): ovverride unset-value to 2.0.1
    
    * Revert "chore(rudder-sdk-react-native-monorepo): ovverride unset-value to 2.0.1"
    
    This reverts commit 05b388746401372b475948cf5787ba10e8abab42.

--- libs/rudder-integration-appsflyer-react-native/android/src/main/java/com/rudderstack/integration/reactnative/appsflyer/RudderIntegrationAppsflyerReactNativeModule.java
+++ libs/rudder-integration-appsflyer-react-native/android/src/main/java/com/rudderstack/integration/reactnative/appsflyer/RudderIntegrationAppsflyerReactNativeModule.java
@@ -215,4 +215,13 @@ public class RudderIntegrationAppsflyerReactNativeModule extends ReactContextBas
         }
     }
 
+    @ReactMethod
+    public void addListener(String eventName) {
+        // Keep: Required for RN built in Event Emitter Calls.
+    }
+
+    @ReactMethod
+    public void removeListeners(Integer count) {
+        // Keep: Required for RN built in Event Emitter Calls.
+    }
 }
