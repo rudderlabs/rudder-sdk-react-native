bb1bd9efd3cac45d39df0c6af6f446781f772578
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2023-08-23 16:30:39 +0530

    feat(rudder-sdk-react-native): add db encyption support in ios module

--- libs/sdk/ios/RNParamsConfigurator.m
+++ libs/sdk/ios/RNParamsConfigurator.m
@@ -78,6 +78,12 @@
     if ([config objectForKey:@"logLevel"]) {
         [configBuilder withLoglevel:[config[@"logLevel"] intValue]];
     }
+    if ([config objectForKey:@"dbEncryption"]) {
+        NSDictionary *dbEncryption = config[@"dbEncryption"];
+        NSString *key = dbEncryption[@"key"];
+        BOOL enable = [dbEncryption[@"enable"] boolValue];
+        [configBuilder withDBEncryption:[[RSDBEncryption alloc] initWithKey:key enable:enable]];
+    }
     return configBuilder;
 }
 
