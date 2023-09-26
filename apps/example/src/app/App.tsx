63f08407366416c8d9f6c03a51cff4d262607812
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2023-09-27 00:21:59 +0530

    chore(example): attempt to reduce duplicate lines

--- apps/example/src/app/App.tsx
+++ apps/example/src/app/App.tsx
@@ -179,46 +179,8 @@ const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: '#ffffff',
   },
-  codeBlock: {
-    backgroundColor: 'rgba(55, 65, 81, 1)',
-    marginVertical: 12,
-    padding: 12,
-    borderRadius: 4,
-  },
-  monospace: {
-    color: '#ffffff',
-    fontFamily: 'Courier New',
-    marginVertical: 4,
-  },
-  comment: {
-    color: '#cccccc',
-  },
-  marginBottomSm: {
-    marginBottom: 6,
-  },
-  marginBottomMd: {
-    marginBottom: 18,
-  },
-  marginBottomLg: {
-    marginBottom: 24,
-  },
-  textLight: {
-    fontWeight: '300',
-  },
-  textBold: {
-    fontWeight: '500',
-  },
   textCenter: {
     textAlign: 'center',
-  },
-  text2XS: {
-    fontSize: 12,
-  },
-  textXS: {
-    fontSize: 14,
-  },
-  textSm: {
-    fontSize: 16,
   },
   textMd: {
     fontSize: 18,
@@ -229,37 +191,9 @@ const styles = StyleSheet.create({
   textXL: {
     fontSize: 48,
   },
-  textContainer: {
-    marginVertical: 12,
-  },
-  textSubtle: {
-    color: '#6b7280',
-  },
   section: {
     marginVertical: 24,
     marginHorizontal: 12,
-  },
-  shadowBox: {
-    backgroundColor: 'white',
-    borderRadius: 24,
-    shadowColor: 'black',
-    shadowOpacity: 0.15,
-    shadowOffset: {
-      width: 1,
-      height: 4,
-    },
-    shadowRadius: 12,
-    padding: 24,
-    marginBottom: 24,
-  },
-  listItem: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  listItemTextContainer: {
-    marginLeft: 12,
-    flex: 1,
   },
   appTitleText: {
     paddingTop: 12,
@@ -279,23 +213,12 @@ const styles = StyleSheet.create({
     color: '#ffffff',
     marginLeft: 12,
   },
-  heroText: {
-    color: '#ffffff',
-    marginVertical: 12,
-  },
   whatsNextButton: {
     backgroundColor: '#ffffff',
     paddingVertical: 16,
     borderRadius: 8,
     width: '50%',
     marginTop: 24,
-  },
-  learning: {
-    marginVertical: 12,
-  },
-  love: {
-    marginTop: 12,
-    justifyContent: 'center',
   },
 });
 
