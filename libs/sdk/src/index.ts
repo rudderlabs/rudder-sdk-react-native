2d019329d994b64b06dab13b198c74212909e893
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2023-10-16 17:09:29 +0530

    refactor(rudder-plugin-db-encryption-react-native): dbEncryption plugin implementation

--- libs/sdk/src/index.ts
+++ libs/sdk/src/index.ts
@@ -1,5 +1,6 @@
 import rudderClient from './RudderClient';
 import { RUDDER_LOG_LEVEL } from './Logger';
+import IDBEncryption from './IDBEncryption';
 
-export { RUDDER_LOG_LEVEL };
+export { RUDDER_LOG_LEVEL, IDBEncryption };
 export default rudderClient;
