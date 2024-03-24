fa7070f20b076f0d1f38409b7d1a8cfa44d07a97
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-03-24 21:27:01 +0530

    Revert "chore(rudder-sdk-react-native): address cognitive complexity issue"
    This reverts commit f578130ce30ad852b8025fee9bb4d45f1b7315ca.

--- libs/sdk/src/Util.ts
+++ libs/sdk/src/Util.ts
@@ -1,37 +1,38 @@
 import { logError } from './Logger';
 
-function convertNestedObjects(obj: Record<string, unknown>): Record<string, unknown> {
-  const updatedObj: Record<string, unknown> = {};
+export function filterNaN(value: Record<string, unknown> | null): Record<string, unknown> | null {
+  if (value == null) return null;
 
-  try {
-    if (typeof obj === 'object') {
-      for (const [key, val] of Object.entries(obj)) {
-        if (typeof val === 'number' && isNaN(val)) {
-          // Drop NaN values, as it is not supported by the React Native bridge layer.
-          continue;
-        } else if (val === null) {
-          updatedObj[key] = null;
-        } else if (typeof val === 'object') {
-          if (!Array.isArray(val)) {
-            updatedObj[key] = convertNestedObjects(val as Record<string, unknown>);
+  function convertNestedObjects(obj: Record<string, unknown>): Record<string, unknown> {
+    const updatedObj: Record<string, unknown> = {};
+
+    try {
+      if (typeof obj === 'object') {
+        for (const [key, val] of Object.entries(obj)) {
+          if (typeof val === 'number' && isNaN(val)) {
+            // Drop NaN values, as it is not supported by the React Native bridge layer.
+            continue;
+          } else if (val === null) {
+            updatedObj[key] = null;
+          } else if (typeof val === 'object') {
+            if (!Array.isArray(val)) {
+              updatedObj[key] = convertNestedObjects(val as Record<string, unknown>);
+            } else {
+              updatedObj[key] = val.map((item) => {
+                return convertNestedObjects(item as Record<string, unknown>);
+              });
+            }
           } else {
-            updatedObj[key] = val.map((item) => {
-              return convertNestedObjects(item as Record<string, unknown>);
-            });
+            updatedObj[key] = val;
           }
-        } else {
-          updatedObj[key] = val;
         }
       }
+    } catch (error) {
+      logError('An error occurred while filtering NaN values: ' + error);
     }
-  } catch (error) {
-    logError('An error occurred while filtering NaN values: ' + error);
+
+    return updatedObj;
   }
 
-  return updatedObj;
-}
-
-export function filterNaN(value: Record<string, unknown> | null): Record<string, unknown> | null {
-  if (value === null) return null;
   return convertNestedObjects(value);
 }
