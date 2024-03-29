4aeea35c7c0fb72d15dcd09fcbcd75bfb5dd73d9
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-03-29 19:07:28 +0530

    chore(example): address sonarcloud issue

--- apps/example/src/main-web.tsx
+++ apps/example/src/main-web.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom/client';
 
 import App from './app/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
     <App />
