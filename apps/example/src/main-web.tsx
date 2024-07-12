a9ce2f8c2a8141c154ecd3a23859093de861400a
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-07-13 00:43:20 +0530

    chore(example): update sample app
    This is necessary to fix the build issue in Android and iOS

--- apps/example/src/main-web.tsx
+++ apps/example/src/main-web.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom/client';
 
 import App from './app/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
     <App />
