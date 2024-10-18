971f989a4db8957796e012be80865128624b8a96
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-10-19 01:17:00 +0530

    chore: provide default GZIP value in the message

--- libs/sdk/src/RudderClient.ts
+++ libs/sdk/src/RudderClient.ts
@@ -4,7 +4,7 @@ import AsyncLock from 'async-lock';
 import { configure } from './RudderConfiguration';
 import bridge, { Configuration } from './NativeBridge';
 import { logInit, logDebug, logError, logWarn } from './Logger';
-import { SDK_VERSION } from './Constants';
+import { SDK_VERSION, ENABLE_GZIP } from './Constants';
 import IRudderContext from './IRudderContext';
 import { filterNaN } from './FilterNaN';
 
@@ -79,7 +79,9 @@ function validateConfiguration(configuration: Configuration) {
     delete configuration.collectDeviceId;
   }
   if (configuration.enableGzip && typeof configuration.enableGzip != 'boolean') {
-    logWarn("setup : 'enableGzip' must be a boolean. Falling back to the default value");
+    logWarn(
+      `setup : 'enableGzip' must be a boolean. Falling back to the default value ${ENABLE_GZIP}`,
+    );
     delete configuration.enableGzip;
   }
 }
