0b19915e67ae266811798ec01d51d9473909dde0
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-05-29 16:16:16 +0530

    refactor: reorder comments for clarity in RNRudderSdkModule header

--- libs/sdk/ios/RNRudderSdkModule.h
+++ libs/sdk/ios/RNRudderSdkModule.h
@@ -1,14 +1,15 @@
-// Thanks to this guard, we won't import this header when we build for the old architecture.
-
 #import "RNRudderSdkModuleImpl.h"
 
+  // Thanks to this guard, we won't import this header when we build for the old architecture.
 #ifdef RCT_NEW_ARCH_ENABLED
 
+  // New Architecture
 #import "RNRudderSdkModuleSpec/RNRudderSdkModuleSpec.h"
 @interface RNRudderSdkModule : NSObject <NativeRudderBridgeSpec> {
 
 #else
 
+  // Legace Archietecture
 #if __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
 #else
